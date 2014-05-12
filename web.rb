578cc89846277a833f5957e7bddb7c7800aa04cc
Keloran <keloran@develbox.info>
2014-05-12 20:46:30 +0000

    add mobile js

--- web.rb
+++ web.rb
@@ -4,13 +4,16 @@ require 'twitter'
 
 set :bind, '0.0.0.0'
 
+=begin
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "GfKMNk5CrFBC6t8cBmtyQiWho"
   config.consumer_secret     = "Ir4Jj8leIKoxt9dk9DmgUPNCxzkgWGEzJkX3TpyKbySTbgUSNJ"
   config.access_token        = "2449575319-0hwDFu90p1uGVfs8UvTKmREAwIyP0RDFF56Pd50"
   config.access_token_secret = "8pHdszTOjHDqxDFHAs2QJV6fJ9hwQRLReS0lCf9Oy3q1D"
 end
+=end
 
 get '/' do
-  haml :index, :locals => {:client => client}
+  #haml :index, :locals => {:client => client}
+  haml :index
 end
